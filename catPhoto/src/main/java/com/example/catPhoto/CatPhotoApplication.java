b6728552786eda04b65997f930e79f0dc09e7705
jtakashi <jwork09110525@gmail.com>
2021-08-13 11:32:54 +0900

    firstcommit catphoto

---
+++ catPhoto/src/main/java/com/example/catPhoto/CatPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.catPhoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatPhotoApplication.class, args);
+	}
+
+}
---
+++ catPhoto/src/test/java/com/example/catPhoto/CatPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catPhoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
